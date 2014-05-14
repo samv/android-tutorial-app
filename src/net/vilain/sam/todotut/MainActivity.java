e6ce5e7e32c0636bfcf5aadc9bfb06ea548d515f
Sam Vilain <sam@vilain.net>
2014-05-13 22:33:32 -0700

    Hello, nurse!
    State at about 5min in to http://vimeo.com/72473627

---
+++ src/net/vilain/sam/todotut/MainActivity.java
@@ -0,0 +1,16 @@
+package net.vilain.sam.todotut;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
