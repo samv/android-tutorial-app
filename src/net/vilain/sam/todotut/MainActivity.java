30b4c0629784d7f7f5032c5fcd0a04bb15a7ea4a
Sam Vilain <sam@vilain.net>
2014-05-14 19:49:32 -0700

    Add etWords class property
    23:40

--- src/net/vilain/sam/todotut/MainActivity.java
+++ src/net/vilain/sam/todotut/MainActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
+    public EditText etWords;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
