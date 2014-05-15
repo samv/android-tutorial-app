1edb2243d1f55cc3bb57e8f48dfcecee52a8c84c
Sam Vilain <sam@vilain.net>
2014-05-14 22:37:57 -0700

    Create basic list of items and display them in the ListView
    Slide 14

--- src/net/vilain/sam/todo/TodoActivity.java
+++ src/net/vilain/sam/todo/TodoActivity.java
@@ -1,16 +1,32 @@
 package net.vilain.sam.todo;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TodoActivity extends Activity
 {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvTodoItems;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+
+        lvTodoItems = (ListView) findViewById(R.id.lvTodoItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvTodoItems.setAdapter(itemsAdapter);
+        items.add("Write an app");
+        items.add("?");
+        items.add("Profit");
     }
 
     @Override
