fff3c00cc2f9f15913c1522daf7e51c82bfacdbe
Sam Vilain <sam@vilain.net>
2014-05-14 21:45:02 -0700

    New "artwork" for Todo App (a bee)
    Apparently this bee is public domain.
    
    From http://pixabay.com/en/happy-cartoon-bee-hands-wings-44524/

---
+++ src/net/vilain/sam/todo/TodoActivity.java
@@ -0,0 +1,15 @@
+package net.vilain.sam.todo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TodoActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
