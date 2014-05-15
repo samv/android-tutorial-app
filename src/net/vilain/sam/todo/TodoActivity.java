db9db042ba96e0c4b8537dfa481ff2267f60bfba
Sam Vilain <sam@vilain.net>
2014-05-14 22:31:16 -0700

    Add a menu
    I saw this in the example and thought I'd quickly check it out.

--- src/net/vilain/sam/todo/TodoActivity.java
+++ src/net/vilain/sam/todo/TodoActivity.java
@@ -2,14 +2,20 @@ package net.vilain.sam.todo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class TodoActivity extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.todo, menu);
+        return true;
     }
 }
