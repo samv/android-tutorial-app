2cf7009adcab3869973241bf06cbb9e6d10eccf7
Sam Vilain <sam@vilain.net>
2014-05-14 21:24:23 -0700

    Update TextView when submit button is pressed
    28:44

--- src/net/vilain/sam/todotut/MainActivity.java
+++ src/net/vilain/sam/todotut/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
     public EditText etWords;
+    public TextView tvLabel;
 
     /** Called when the activity is first created. */
     @Override
@@ -19,11 +21,13 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         etWords = (EditText)findViewById(R.id.etWords);
+        tvLabel = (TextView)findViewById(R.id.tvLabel);
     }
 
     public void onSubmit(View v) {
         String fieldValue = etWords.getText().toString();
-      // do something when the button is clicked
+        // do something when the button is clickedEditText
+        tvLabel.setText(fieldValue);
         Toast.makeText(this, fieldValue, Toast.LENGTH_SHORT).show();
     }
 }
